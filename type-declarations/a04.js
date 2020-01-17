0a80ac7d4ff1663d9da0de7702b763885956212b
git-hub-user-testing <59990448+git-hub-user-testing@users.noreply.github.com>
2020-01-17 19:46:25 +0530

    Add files via upload

---
+++ type-declarations/a04.js
@@ -0,0 +1,8 @@
+var a;
+var b;
+var c;
+var d;
+a = 10;
+b = true;
+c = "Virtusa";
+console.log(typeof d);
---
+++ type-declarations/a04.ts
@@ -0,0 +1,10 @@
+var a: number;
+var b: boolean;
+var c: string;
+var d;
+
+a = 10;
+b = true;
+c = "Virtusa";
+
+console.log(typeof d);
